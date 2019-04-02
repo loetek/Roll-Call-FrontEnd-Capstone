ef3ec33f1431b811259ba6419d93afe296070249
JD <jdwheeler0@gmail.com>
2019-04-02 16:36:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const dbPath = './API/db.json';
+const dbPath = './API/datatbase.json';
 const dbRoutes = Object.keys(require(dbPath)).map(r => '/' + r);
 const jsonServer = require('json-server');
 const server = jsonServer.create();
